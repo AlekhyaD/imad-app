bdf1aa448a3551e7b6616ebc72db9282e1e8ddb9
AlekhyaD <30995873+AlekhyaD@users.noreply.github.com>
2017-08-23 18:08:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName',function(req,res)
 {
     var articleName=req.params.articleName;
-   res.send(createTemplate(Articles[articleName]));
+   res.send(createTemplate(articles[articleName]));
 });
 
 
