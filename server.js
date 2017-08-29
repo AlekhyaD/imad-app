3858d4717429751a6b7997d7fa9876963c780104
AlekhyaD <30995873+AlekhyaD@users.noreply.github.com>
2017-08-29 21:07:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/:articleName',function(req,res)
    res.send(createTemplate(articles[articleName]));
 });
 app.get('/ui/main.js',function(req,res){
-    res.sendFile(path.join(_dirname,'ui','main.js'));
+    res.sendFile(path.join(__dirname,'ui','main.js'));
 });
 
 app.get('/ui/style.css', function (req, res) {
